631322ec132b604f503d4d7e00809da482ebd018
Raildo <Raildo@LG-PC.Home>
2015-06-26 14:56:29 -0400

    Adição da entidade Sexo

--- semeru_jsf_maven/src/main/java/br/com/projeto/model/entities/Pessoa.java
+++ semeru_jsf_maven/src/main/java/br/com/projeto/model/entities/Pessoa.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 
@@ -38,6 +40,10 @@ public class Pessoa implements Serializable {
     @Column(name = "DataDeCadastro", nullable = false)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataDeCadastro;
+    
+    @ManyToOne(optional=false)
+    @ForeignKey(name = "PessoaSexo")
+    private Pessoa pessoa;
 
     public Pessoa() {
     }
---
+++ semeru_jsf_maven/src/main/java/br/com/projeto/model/entities/Sexo.java
@@ -0,0 +1,87 @@
+package br.com.projeto.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Raildo
+ */
+@Entity
+@Table(name="sexo")
+public class Sexo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "IdSexo", nullable = true)
+    private Integer idSexo;
+    @Column(name = "Descricao", unique = true, nullable = false, length = 9)
+    private String descricao;
+
+    @OneToMany(mappedBy = "sexo", fetch = FetchType.LAZY)
+    @ForeignKey(name = "PessoaSexo")
+    private List<Pessoa> pessoas; 
+    
+    public Sexo() {
+    }
+
+    public Integer getIdSexo() {
+        return idSexo;
+    }
+
+    public void setIdSexo(Integer idSexo) {
+        this.idSexo = idSexo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.idSexo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Sexo other = (Sexo) obj;
+        if (!Objects.equals(this.idSexo, other.idSexo)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
